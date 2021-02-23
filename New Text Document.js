c0b74fdc08be00362b7024278dcf15e507b7d192
walid <portawalid@gmail.com>
2021-02-23 21:01:08 +0100

    dada

---
+++ New Text Document.js
@@ -0,0 +1,11 @@
+function show() {
+    var output=document.getElementById("zz")
+    if( zz>10){
+        alert("true");
+    }
+    else
+    alert("false");
+    
+
+   
+}
--- app.js
+++ app.js
@@ -17,37 +17,35 @@ function close(){
 }
 
 
+// ====================================  For contact popup
+
+
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+    // For Full name
+    var nametxt= document.getElementById("name").value,
+        shownametxt= document.getElementById("nametxt");
+         shownametxt.textContent="Your name is: "+nametxt;
+    // For email
+    var nametxt= document.getElementById("email").value,
+         shownametxt= document.getElementById("emailtxt");
+          shownametxt.textContent="Your Email is: "+nametxt; 
+    // For Phone
+    var nametxt= document.getElementById("phone").value,
+          shownametxt= document.getElementById("phonetxt");
+           shownametxt.textContent="Your Phone is: "+nametxt;    
+    // For msg
+    var nametxt= document.getElementById("msg").value,
+          shownametxt= document.getElementById("msgtxt");
+           shownametxt.textContent="Your Msg is: "+nametxt;       
+          
+  }
+
+
+
+
 // ====================================
-
-
-// function liste(){
-//     var i=formulaire.type.selectedIndex;
-
-// if(i==0){
-//     for(j=1; j<i.length; j++)
-//     formulaire.energy.options[j].text="";
-// }    
-// else{
-//     switch(i)
-//     {
-//         case 1 : var vi =new Array("Electrique","Essence","",""); break;
-//         case 2 : var vi =new Array("Electrique","Hybride","Essence","diesel"); break;
-//         case 3 : var vi =new Array("Hybride","Essence","diesel",""); break;
-//         case 4 : var vi =new Array("Hybride","Essence","diesel",""); break;
-//         case 5 : var vi =new Array("diesel","","",""); break;
-//         case 6 : var vi =new Array("diesel","Essence","",""); break;
-//         case 7 : var vi =new Array("diesel","","",""); break;
-//     }
-//     for(k =0;k<4;k++)
-//     formulaire.energy.options[k].text=vi[k];
-    
-// }
-// formulaire.energy.selectedIndex=0;
-// }
-
-// if(i==1 && vi[0]){
-//     alert("hiiiii");
-// }
 
 
 
@@ -59,12 +57,14 @@ function myliste() {
 
     var i = formulaire.vehicule.selectedIndex;
 
-    for (j = 1; j < 5; j++) formulaire.carburant.options[j].text = "";
+    for (j = 1; j < 5; j++)
+     formulaire.carburant.options[j].text = "";
 
 
 
     if (i == 0) {
-        for (j = 1; j < 4; j++) formulaire.carburant.options[j].text = "";
+        for (j = 1; j < 4; j++) 
+         formulaire.carburant.options[j].text = "";
 
     } else {
         switch (i) {
@@ -203,30 +203,48 @@ function calcule() {
 }
 
 
-function togglePopup(){
-    document.getElementById("popup-1").classList.toggle("active");
-    var nametxt= document.getElementById("name").value,
-        shownametxt= document.getElementById("nametxt");
-         shownametxt.textContent="Your name is: "+nametxt;
 
-    var nametxt= document.getElementById("email").value,
-         shownametxt= document.getElementById("emailtxt");
-          shownametxt.textContent="Your Email is: "+nametxt; 
-    var nametxt= document.getElementById("phone").value,
-          shownametxt= document.getElementById("phonetxt");
-           shownametxt.textContent="Your Phone is: "+nametxt;       
-    var nametxt= document.getElementById("msg").value,
-          shownametxt= document.getElementById("msgtxt");
-           shownametxt.textContent="Your Msg is: "+nametxt;       
-          
-  }
 
-// function getnametxt(){
-//     var nametxt= document.getElementById("name").value,
-//         shownametxt= document.getElementById("pp");
-//          shownametxt.textContent="your name is :"+nametxt;
-        
-//      console.log(nametxt);
+
+
+
+
+
+
+
+
+
+
+
+
+//   =================================================
+
+// function liste(){
+//     var i=formulaire.type.selectedIndex;
+
+// if(i==0){
+//     for(j=1; j<i.length; j++)
+//     formulaire.energy.options[j].text="";
+// }    
+// else{
+//     switch(i)
+//     {
+//         case 1 : var vi =new Array("Electrique","Essence","",""); break;
+//         case 2 : var vi =new Array("Electrique","Hybride","Essence","diesel"); break;
+//         case 3 : var vi =new Array("Hybride","Essence","diesel",""); break;
+//         case 4 : var vi =new Array("Hybride","Essence","diesel",""); break;
+//         case 5 : var vi =new Array("diesel","","",""); break;
+//         case 6 : var vi =new Array("diesel","Essence","",""); break;
+//         case 7 : var vi =new Array("diesel","","",""); break;
+//     }
+//     for(k =0;k<4;k++)
+//     formulaire.energy.options[k].text=vi[k];
+    
+// }
+// formulaire.energy.selectedIndex=0;
 // }
 
+// if(i==1 && vi[0]){
+//     alert("hiiiii");
+// }
 
